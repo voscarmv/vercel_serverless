8982a51e042ac7402e510823274d0442732b9f87
Oscar Mier <voscarmv@gmail.com>
2025-09-19 01:16:14 -0500

    Use the env var created by Nile DB on Vercel

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
+    connection: process.env.POSTGRES_URL,
     migrations: {
       tableName: 'knex_migrations'
     }
